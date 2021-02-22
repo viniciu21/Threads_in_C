5dccebd3d3a0a4b2bcd6603fcc06158d68bae5f3
ubuntovini <viniciusoliveiras12@hotmail.com.br>
2021-02-22 15:57:51 -0300

    trabalho (ruim)

--- NThreads_sync.c
+++ NThreads_sync.c
@@ -2,48 +2,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#define NTHREADS 5
-
-pthread_t threads[NTHREADS];
-int status, i;
 void *thread_return;
 
+struct testes{
+    int a;
+    int b;
+};
+
+struct argsThreads{
+    int a;
+    int b;
+    int h;
+    int i;
+};
+
+double areasoma = 0;
 int soma = 0;
 
-void* hello_world(void *tid){
-    if((int)(size_t)tid > 0){
-        pthread_join(threads[(size_t)tid-1], &thread_return);
-        printf("Esta é a Thread %d. A Thread %d terminou. ", (int) (size_t)tid, (int) (size_t)tid-1);
-    }
-    else
-        printf("Esta é a primeira Thread.");     
-    
-    soma += 1;
-    printf("O valor de soma eh: %d\n", soma);
-    sleep(1);
+void* hello_world(void *argumentos){
+
+    struct argsThreads *args = (struct argsThreads *)argumentos;
+
+    printf("%d %d %d %d \n", args->a, args->b, args->h, args->i);
+
+    soma += args->i;
+
     pthread_exit(NULL);
 }
 
 int main(int argc, char const *argv[]) {
     
-    
-    for (i = 0; i < NTHREADS; i++){
-        printf("Processo principal criando thread #%d\n", i);
-        status = pthread_create(&threads[i], NULL, hello_world, (void *)(size_t) i);
+// atoi(argv[1]);
+// atoi(argv[2]);
+    int numero_threads = 5;
+    int numero_trapesios = 4;
 
-        if(status!=0){
-            printf("Erro na criação da thread. Codigo de Erro: %d\n", status);
-            return 1;
-        }
+    pthread_t threads_trapezios[numero_threads];
+
+    struct testes test1;
+    test1.a = 0;
+    test1.b = 12;
+    int h = (test1.b - test1.a)/numero_trapesios;
+    pthread_t threads[numero_threads];
+
+    struct argsThreads args;
+    args.a = test1.a;
+    args.b = test1.b;
+    args.h = h;
+    args.i = 0;
+
+    for (int i = 0; i < numero_threads; i++){
+        args.i = i;
+        printf("%d %d\n", i, args.i);
+        pthread_create(&threads[i], NULL, hello_world, (void * ) &args);
     }
 
-    printf("Esperando Thread %d finalizar...\n", i-1);
-    pthread_join(threads[i-1], &thread_return);
-    printf("Thread %d finalizada\n", i-1);
-
-    printf("Processo pai vai finalizar e soma = %d\n", soma);
-
     
+    for(int i = 0; i < numero_threads; i++){
+        pthread_join(threads[i], &thread_return);
+    }
+
+    printf("soma %d\n", soma);
+
     return 0;
 }
